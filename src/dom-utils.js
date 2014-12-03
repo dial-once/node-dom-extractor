0d11b3730587520eb9735ab9341764cfd40a42b3
Jonathan <koren@pukogames.com>
2014-12-03 14:18:56 +0100

    Add travis build file and fixed lint issues

--- src/dom-utils.js
+++ src/dom-utils.js
@@ -1,13 +1,14 @@
 var utils = module.exports;
 
 utils.isValidUrl = function(url) {
+	'use strict';
 	if (url === undefined) return false;
 
-	var expression = "^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|www\\.){1}([0-9A-Za-z-\\.@:%_\+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?"
+	var expression = "^(http[s]?:\\/\\/(www\\.)?|ftp:\\/\\/(www\\.)?|(www\\.)?){1}([0-9A-Za-z-\\.@:%_+~#=]+)+((\\.[a-zA-Z]{2,3})+)(/(.)*)?(\\?(.)*)?";
 	var regex = new RegExp(expression);
 	if (url.match(regex)) {
 		return true;
 	} else {
 		return false;
 	}
-}
+};
