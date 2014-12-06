8d6fb83a7f018e4fcc191ca80ef6b25039acdc4d
PuKoren <koren@pukogames.com>
2014-12-06 13:08:16 +0100

    Allow # selectors, using |sharp|

--- src/dom-extractor.js
+++ src/dom-extractor.js
@@ -1,31 +1,52 @@
-var utils 		= require('./dom-utils'),
-	jsdom 		= require('jsdom'),
-	juice 		= require('juice'),
-	NodeCache 	= require('node-cache'),
-	extractor 	= module.exports;
+var utils = require('./dom-utils'),
+	jsdom = require('jsdom'),
+	juice = require('juice'),
+	NodeCache = require('node-cache'),
+	extractor = module.exports;
 
-var nodeCache 	= new NodeCache();
+var nodeCache = new NodeCache();
 
 function jsdomCallback(cacheKey, document, callback) {
+	if (document.body === null) {
+		callback('');
+		return;
+	}
+
 	juice.juiceDocument(document, {
 		url: 'fake'
 	}, function() {
-		if(cacheKey !== null){
+
+		if (cacheKey !== null) {
 			nodeCache.set(cacheKey, document.body.innerHTML);
 		}
 		callback(document.body.innerHTML);
 	});
 }
 
-extractor.fetch = function(data, selector, callback) {
-	var cacheKey 	= null;
-	var isValidUrl 	= utils.isValidUrl(data);
+function cleanRelativePathes($, absoluteUrl) {
+	$('link').each(function() {
+		$(this).attr('href', utils.relToAbs(absoluteUrl, $(this).attr('href')));
+	});
+	$('img').each(function() {
+		$(this).attr('src', utils.relToAbs(absoluteUrl, $(this).attr('src')));
+	});
+	$('a').each(function() {
+		$(this).attr('href', utils.relToAbs(absoluteUrl, $(this).attr('href')));
+	});
+	$('form').each(function() {
+		$(this).attr('action', utils.relToAbs(absoluteUrl, $(this).attr('action')));
+	});
+}
 
-	if(isValidUrl){
-		cacheKey 		= data + '#' + selector;
+extractor.fetch = function(data, selector, callback) {
+	var cacheKey = null;
+	var isValidUrl = utils.isValidUrl(data);
+
+	if (isValidUrl) {
+		cacheKey = data + '#' + selector;
 		var cachedValue = nodeCache.get(cacheKey);
-		
-		if(cachedValue[cacheKey] !== undefined){
+
+		if (cachedValue[cacheKey] !== undefined) {
 			callback(cachedValue[cacheKey]);
 			return;
 		}
@@ -34,30 +55,22 @@ extractor.fetch = function(data, selector, callback) {
 	if (selector instanceof Function) {
 		callback = selector;
 		selector = 'body';
-	} else if (typeof selector !== 'string') {
+	} else if (typeof selector !== 'string' || selector === '/') {
 		selector = 'body';
+	} else {
+		selector = selector.replace('\\', '');
 	}
 
 	var jsdomconfig = {
 		scripts: ["http://code.jquery.com/jquery.js"],
 		done: function(errors, window) {
 			var $ = window.$;
+
 			$('body').html($(selector).wrap('<span/>').parent().html());
 			$('script').remove();
 
 			if (isValidUrl) {
-				$('link').each(function() {
-					$(this).attr('href', utils.relToAbs(data, $(this).attr('href')));
-				});
-				$('img').each(function() {
-					$(this).attr('src', utils.relToAbs(data, $(this).attr('src')));
-				});
-				$('a').each(function() {
-					$(this).attr('href', utils.relToAbs(data, $(this).attr('href')));
-				});
-				$('form').each(function() {
-					$(this).attr('action', utils.relToAbs(data, $(this).attr('action')));
-				});
+				cleanRelativePathes($, data);
 			}
 			jsdomCallback(cacheKey, window.document, callback);
 		}
@@ -76,7 +89,7 @@ extractor.fetch = function(data, selector, callback) {
 	} else {
 		jsdomconfig.html = data;
 	}
-	
+
 	jsdom.env(jsdomconfig);
 };
 
@@ -85,6 +98,7 @@ extractor.middleware = function(options) {
 	return function(req, res, next) {
 		var params = require('url').parse(req.url, true).query;
 		if (params.url !== undefined && params.selector !== undefined) {
+			params.selector = params.selector.replace('|sharp|', '#');
 			extractor.fetch(params.url, params.selector, function(response) {
 				res.write(response);
 				res.end();
