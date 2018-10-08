70b61b08f4f6b6b60e84a7a52268a1fb41a7eee7
steven-heng <steven.heng@epitech.eu>
2018-10-08 23:14:41 +0800

    Re-fix indents

--- test/sample.js
+++ test/sample.js
@@ -3,46 +3,46 @@ const domextractor = require('./../src/dom-extractor');
 
 domextractor.fetch("<html><head><style>.b{background-color:#eee}</style></head><body><div class='b'>"
   + "<a href='ok'>ok mec</a></div><div class='c'>ok</div></body></html>",
-'.b',
-(data) => {
-  assert.notEqual(data, undefined);
-});
+  '.b',
+  (data) => {
+    assert.notEqual(data, undefined);
+  });
 
 domextractor.fetch("<html><head><style>.b{background-color:#eee}</style></head><body><div class='b'>"
   + "<a href='ok'>ok mec</a></div><div class='c'>ok</div></body></html>", {
-  selector: '.b',
-  innerText: true
-}, (b) => {
-  assert.notEqual(b, undefined);
-});
+    selector: '.b',
+    innerText: true
+  }, (b) => {
+    assert.notEqual(b, undefined);
+  });
 
 domextractor.fetch("<html><head><style>.b{background-color:#eee}</style></head><body><div class='b'>"
   + "<a href='ok'>ok mec</a></div><div class='c'>ok</div></body></html>", {
-  selector: '.b',
-  innerText: true,
-  inlineCss: false
-}, (c) => {
-  assert.notEqual(c, undefined);
-});
+    selector: '.b',
+    innerText: true,
+    inlineCss: false
+  }, (c) => {
+    assert.notEqual(c, undefined);
+  });
 
 domextractor.fetch("<html><head><style>.b{background-color:#eee}</style></head><body><div class='b'>"
   + "<a href='ok'>ok mec</a></div><div class='c'>ok</div></body></html>", {
-  selector: '.b',
-  innerText: false,
-  inlineCss: false
-}, (d) => {
-  assert.notEqual(d, undefined);
-});
+    selector: '.b',
+    innerText: false,
+    inlineCss: false
+  }, (d) => {
+    assert.notEqual(d, undefined);
+  });
 
 domextractor.fetch("<html><head><style>.b{background-color:#eee}</style></head><body><div class='b'>"
   + "<a href='ok'>ok mec</a></div><div class='c'>ok</div></body></html>", {
-  selector: '.b',
-  innerText: false,
-  inlineCss: false,
-  removeLinks: true
-}, (e) => {
-  assert.notEqual(e, undefined);
-});
+    selector: '.b',
+    innerText: false,
+    inlineCss: false,
+    removeLinks: true
+  }, (e) => {
+    assert.notEqual(e, undefined);
+  });
 
 domextractor.fetch('http://www.camif.fr/espace-service/livraison-gratuite.html', {
   selector: 'div.contentpconseil',
